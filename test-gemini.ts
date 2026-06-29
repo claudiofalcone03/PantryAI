24435079f9752561f052018842e39a17a83ae594
Claudio <cfalcone98@gmail.com>
2026-06-29 17:13:11 +0200

    Aggiunta Genkit CLI

--- test-gemini.ts
+++ test-gemini.ts
@@ -2,10 +2,10 @@ import { generaRicetta } from './lib/genkit/genkit';
 
 async function main() {
     console.log("Inviando la richiesta a Gemini...");
-    const ingredienti = ["Patatine", "Mozzarella", "Parmigiano", "panna", "wurstel"];
+    const ingredienti = ["Zucchine", "Pomodori", "Formaggio", "Uova", "Pesto"];
     const ricetta = await generaRicetta(ingredienti);
 
-    console.log("\n--- Risposta di Gemini ---");
+    console.log("\n--- Risposta di Gemini  alle 17:10 ---");
     console.log(ricetta);
     console.log("--------------------------");
 }
